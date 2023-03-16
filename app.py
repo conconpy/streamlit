46adf3aea24c34c195b1737647579466dfae3897
ConPy <66801098+conconpy@users.noreply.github.com>
2023-03-16 10:11:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@
 
 import streamlit
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
